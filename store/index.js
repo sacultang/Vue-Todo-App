f2323ae3f953484c37b198d299fa52621866c132
sacultang <85508157+sacultang@users.noreply.github.com>
2021-07-12 22:26:18 +0900

    vuex-store-ver1
    Vuex를 설치하고 import

---
+++ store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import todoApp from './todoApp'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    // strict:true //배포할때는 false로 해야함
+
+    strict: process.env.NODE_ENV !== 'production',
+    modules: {
+        todoApp
+    }
+
+})
